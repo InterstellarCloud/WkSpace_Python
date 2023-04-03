81fe7d34680d3692f3b38e37e86301d88b69b7bc
InterstellarCloud <nft188@126.com>
2023-04-03 20:56:56 +0800

    first commit from localPC

---
+++ hello.py
@@ -0,0 +1,13 @@
+#此行是注释......
+print(" I'm OK!")
+print('I\'m \"OK\"!')
+print('300*5 =',300*5)
+sname=input('Please write your sname : ')
+print('print  name =',sname)
+sname
+#此行是注释......
+aa=-6000
+if aa>0:
+    print('aa是正数：',aa)
+else:
+    print('aa是负数：',aa)
